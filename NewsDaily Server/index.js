0ecaff197e7216ffecac28d98172b4321950543b
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-07-12 22:34:45 +0600

    first commit

---
+++ NewsDaily Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Hello from Express server with MongoDB Atlas!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
